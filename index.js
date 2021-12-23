982cf2bab13debae886e49a7816ad185a72d9b4a
BlackMan✓ <78313191+BlackMan69x@users.noreply.github.com>
2021-12-23 19:27:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("ODA3MzUwNTM0OTAxMDcxOTMy.YB2txw.cxgypkSKJDwIkQmkiqX-uVaqabM");
+client.login("OTIzMjA4NjA5MjYzOTE5MTM0.YcMrBA.C-_GJSx8zDgz6uhs8SLA8lOJcSE");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
